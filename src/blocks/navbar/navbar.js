6fdb9054aa17d810bbdcd76221d37d50ec711485
yarutich <info@devweb.by>
2019-04-04 21:41:00 +0300

    create block about-company

--- src/blocks/navbar/navbar.js
+++ src/blocks/navbar/navbar.js
@@ -1,8 +1,6 @@
 var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("is-active");
-    var navMObileLine = document.querySelector(".nav-mobile-line");
-    navMObileLine.style.backgroundColor = 'red';
 });
 
 
@@ -10,9 +8,13 @@ var hamburgerBox = document.querySelector(".hamburger-box");
 
 hamburgerBox.onclick = function myFunction() {
     var navMobile = document.getElementById("navMobile");
+    var navMObileLine = document.querySelector(".nav-mobile-line");
+    
     if (navMobile.style.display === "block") {
         navMobile.style.display = "none";
+        navMObileLine.style.backgroundColor = '#42A2D6';
     } else {
+        navMObileLine.style.backgroundColor = '#fff';
         navMobile.style.display = "block";
     }
 }
