ed71030eb17770b2397ade330e55232322bcd36f
Yarn <efermerm@gmail.com>
2019-04-10 20:57:09 +0300

    update

--- src/blocks/about-company/about-company.js
+++ src/blocks/about-company/about-company.js
@@ -1,18 +1,1 @@
-// import Glide from '@glidejs/glide';
-
-// new Glide('.glide').mount();
-
-modules.define('about-company', ['i-bem-dom'], function(provide, bemDom) {
-
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
-
-});
 
